ffb2ea9a4ad5bee04cd57bd21e4b20b4d3ea63f7
ANGADE <ANGADE@ANGADE-T490.in.oracle.com>
2023-03-31 15:01:55 +0530

    mvnjava11

---
+++ src/main/java/com/anjimaven/anjidemo/AnjidemoApplication.java
@@ -0,0 +1,13 @@
+package com.anjimaven.anjidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnjidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnjidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anjimaven/anjidemo/AnjidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjimaven.anjidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnjidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
